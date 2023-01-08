50a491bf9c23324d10b2c07719cc02a12f506197
Bastien DUMONT <bastienxs@gmail.com>
2023-01-08 17:42:54 +0100

    initial commit

---
+++ src/Avatar.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export const FALLBACK_AVATAR_URL = 'https://cataas.com/cat/says/hello%20world!';
+export const FALLBACK_AVATAR_ALT_TEXT = '@hello-cat';
+
+type AvatarProps = {
+  /** the url of an image to render for the Avatar */
+  url?: string;
+  /** the alt text to use for the url image */
+  alt?: string;
+};
+
+const Avatar = ({
+  url = FALLBACK_AVATAR_URL,
+  alt = FALLBACK_AVATAR_ALT_TEXT,
+}: AvatarProps) => {
+  const [srcToRender, setSrcToRender] = useState(url);
+  return (
+    <img
+      src={srcToRender}
+      alt={alt}
+      onError={() => setSrcToRender(FALLBACK_AVATAR_URL)}
+    />
+  );
+};
+
+export default Avatar;
